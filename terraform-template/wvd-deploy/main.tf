4c07c489d4e4dcb50f08f935a413d19d5984ae6c
Daniel Hibbert <hibbertda@outlook.com>
2020-10-26 15:08:13 -0400

    added wvd plan

--- terraform-template/wvd-deploy/main.tf
+++ terraform-template/wvd-deploy/main.tf
@@ -2,6 +2,15 @@ provider "azurerm" {
     features {}
 }
 
+terraform {
+backend "azurerm" {
+    resource_group_name     = "hbl-demo-env"
+    storage_account_name    = "hblwvdterrstate"
+    container_name          = "wvdstate"
+    key                     = "wvdstate.tfstate"
+}
+}
+
 data "azurerm_client_config" "current" {}
 
 data "azurerm_key_vault" "wvd-kv" {
@@ -33,15 +42,13 @@ module "wvd_hostpool_developers" {
 }
 
 # Create WVD session hosts
-module "wvd_session_hosts" {
-    env                 = var.env    
-    source              = "./modules/wvd_sessionhosts"
-    # rgLocation          = azurerm_resource_group.wvd-hosts.location
-    # rgName              = azurerm_resource_group.wvd-hosts.name
-    keyvault_id         = data.azurerm_key_vault.wvd-kv.id
-    sessionHostCount    = 2
-    network             = var.network
-    hostvm              = var.hostvm
-    adds-join           = var.adds-join
-    wvd-hostpool-name   = module.wvd_hostpool_developers.wvd-hostpool-name
-}
+# module "wvd_session_hosts" {
+#     env                 = var.env    
+#     source              = "./modules/wvd_sessionhosts"
+#     keyvault_id         = data.azurerm_key_vault.wvd-kv.id
+#     sessionHostCount    = 2
+#     network             = var.network
+#     hostvm              = var.hostvm
+#     adds-join           = var.adds-join
+#     wvd-hostpool-name   = module.wvd_hostpool_developers.wvd-hostpool-name
+# }
